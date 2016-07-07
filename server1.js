0f40a1dea1b88905039196a2eca51aff4e99be57
Janice Rodriguez <mate.disc.1ta@gmail.com>
2016-07-07 08:51:47 -0500

    este no es un laboratorio son njotas para mi de cambios a la anterior

---
+++ server1.js
@@ -0,0 +1,30 @@
+//Nos permite acceder a las variables de entorno donde se esta ejecutando el proceso.
+
+// http
+var http = require("http");
+// Obteniendo información del entorno
+// De ejecución con respecto al IP
+// y al puerto que debemos usar en
+// nuestro server.
+var PORT = process.env.PORT || 3000;
+var IP = process.env.IP || '127.0.0.1';
+if(IP == '127.0.0.1'){
+    console.log("> -----Ejecutando de modo local-----");
+}
+// Crear un servidor basico
+var server = http.createServer(function(req, res){
+    // Armar la respuesta http
+    // Armar un encabezado http
+    res.writeHead(200,{
+        "Content-Type" : "text/plain",
+        "Server" : "ITGAM@4.2.4"
+    });
+    // Enviamos la respuesta
+    res.write("Janice Rodriguez Olvera");
+    // Cerrar la conexion
+    res.end();
+});
+// Poner a trabjar al server
+server.listen(PORT,IP,function(){
+    console.log(`> Server listening @http://${IP}:${PORT} ...`);
+});
