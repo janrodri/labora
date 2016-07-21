9e5031c73026842b629467a65f4e3b82a1d035d8
Janice Rodriguez <mate.disc.1ta@gmail.com>
2016-07-21 08:46:43 -0500

    fake Webservice getfortune ok

--- internals/handlers.js
+++ internals/handlers.js
@@ -1,6 +1,6 @@
 //manejadores de rutas virtuales,
 var fechaDeNac = new Date (1987,4,29,7,30);
-//los manejadores encapsulados en un objeto{}
+//los manejadores encapsulados en un objeto{} del metodo
 module.exports = {
     "/edad/Janice-Rodriguez" : function (req, res) {
         res.writeHead(200,{
@@ -15,6 +15,22 @@ module.exports = {
         };
         var jsonResponse = JSON.stringify(objetoRespuesta);
         //Envio la respuesta al cliente
+        res.end(jsonResponse);
+    },
+    //estamos llamando a otra funcion
+    "/getfortune" : function (req, res) {
+        //contestar con un json, y se obtiene mensaje de la fortuna
+        var fortunePaper = {
+            "mensaje" :
+            "La honestidad es un regalo muy caro, no lo esperes de gente barata"
+        };
+        // se configura el contenido de respuesta HTTP
+        res.writeHead(200,{
+            "Content-Type" : "application/json"
+        });
+        // parseando a string el objetoRespuesta de respuesta
+        var jsonResponse = JSON.stringify(fortunePaper);
+        // responde el Objeto
         res.end(jsonResponse);
     }
 };
